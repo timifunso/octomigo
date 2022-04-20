2a6bf9d601722598bb3aebd932ce97b3b52caa08
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-20 05:48:50 +0100

    add global context

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,6 @@ import { useState, useEffect } from "react";
 import GlobalStyles from "./GlobalStyles";
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  async function fetchuser() {
-    try {
-      const fetchData = await fetch(`https://api.github.com/users/${username}`);
-      const res = await fetchData.json();
-      console.log(res);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  useEffect(() => {
-    // fetchuser();
-  }, [username]);
-
   return (
     <>
       <GlobalStyles />
---
+++ src/GlobalContext.jsx
@@ -0,0 +1,27 @@
+import { createContext } from "react";
+
+const GlobalContext = createContext();
+
+export function GlobalContextProvider({ children }) {
+  const [username, setUsername] = useState("");
+
+  async function fetchuser() {
+    try {
+      const fetchData = await fetch(`https://api.github.com/users/${username}`);
+      const res = await fetchData.json();
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    // fetchuser();
+  }, [username]);
+
+  return (
+    <GlobalContext.Provider value={username}>{children}</GlobalContext.Provider>
+  );
+}
+
+export default GlobalContextProvider;
