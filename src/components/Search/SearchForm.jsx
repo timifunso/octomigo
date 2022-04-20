180611a1dc449abd696eaf67232017b41546ffb9
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-20 05:41:28 +0100

    add react-router-dom

---
+++ src/components/Search/SearchForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchForm() {
+  return (
+    <form>
+      <input type="search" placeholder="Enter github username" />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default SearchForm;
---
+++ src/pages/main.jsx
@@ -0,0 +1,11 @@
+import SearchForm from "../components/Search/SearchForm";
+
+function main() {
+  return (
+    <div>
+      <SearchForm />
+    </div>
+  );
+}
+
+export default main;
