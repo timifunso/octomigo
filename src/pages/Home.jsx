639166efaf87fa281939e4cf0041191c9b27ef95
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-20 16:36:58 +0100

    updated Main.jsx to Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import SearchForm from "../components/Search/SearchForm";
+import Wrapper from "../components/Wrapper";
+
+function Home() {
+  return (
+    <div>
+      <Wrapper>
+        <SearchForm />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Home;
