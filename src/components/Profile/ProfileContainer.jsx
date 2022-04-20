d1c7a22b5307ec6ef4256cf6c73f52aa5db1c38e
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-20 18:16:19 +0100

    fixed run time error

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import ReposContainer from "../Repos/ReposContainer";
 import ProfileContainerStyled from "./ProfileContainer.styled";
 import LineIcon from "react-lineicons";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 function ProfileContainer({ data }) {
   const [isLoading, setIsLoading] = useState(true);
@@ -31,9 +31,9 @@ function ProfileContainer({ data }) {
     <section>
       <header>
         <ProfileContainerStyled>
-          <button title="Go back to home" onClick={() => navigate("/")}>
+          {/* <button title="Go back to" onClick={() => navigate("/")}>
             <LineIcon name="arrow-left" />
-          </button>
+          </button> */}
           <img src={avatar_url} alt="" />
           <div className="details">
             <h2>{name}</h2>
