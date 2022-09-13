17e672d65b2cb1b44f182847e35a19ac6d9be4f9
gzkdev <ezihegodswill01@gmail.com>
2022-09-13 16:36:52 +0000

    refactor: update loader component styles and markup

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,9 +1,10 @@
 import styles from "./Loader.module.css";
+import { Refresh } from "iconsax-react";
 
 function Loader() {
   return (
     <div className={styles.container}>
-      <div className={styles.loader}></div>
+      <Refresh size={24} className={styles.loader__icon} />
       <div className={styles.heading}>Wait a sec!</div>
     </div>
   );
